cf018a6e527508427c63fec01a69e3db4a79600f
My <ltquemy@gmail.com>
2026-06-21 16:50:37 +0700

    fix: update settings

--- config/settings.py
+++ config/settings.py
@@ -8,14 +8,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 class Settings(BaseSettings):
     BASE_DIR: ClassVar[Path] = BASE_DIR
 
-    DATA_GENERATOR_CONFIG_PATH: str | None = None
-    TEST_DATA_GENERATOR_CONFIG_PATH: str | None = None
-    DATA_GENERATOR_OUTPUT_PATH: str
+    DATA_GENERATOR_CONFIG_PATH: str = str(
+        BASE_DIR / "a_data_generator" / "config" / "generator_config.yaml"
+    )
+    TEST_DATA_GENERATOR_CONFIG_PATH: str = str(
+        BASE_DIR / "tests" / "a_data_generator" / "test_config.yaml"
+    )
+    DATA_GENERATOR_OUTPUT_PATH: str = str(
+        BASE_DIR / "a_data_generator" / "outputs"
+    )
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
         env_file_encoding="utf-8",
-        extra='ignore',
+        extra="ignore",
     )
 
 
